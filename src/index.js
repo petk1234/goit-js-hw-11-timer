11dffc005406fb769647d5649329ceb6adfdd624
petk1234 <petkun8034@gmail.com>
2021-08-28 12:43:03 +0300

    Final commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,48 @@
-const delay = ms => {
-    const promise = new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(ms);
-        }, ms);
-    });
-    console.log('delay +', ms);
-    return promise;
-};
+
+const refs = {
+    days: document.querySelector("[data-value='days']"),
+    hours: document.querySelector("[data-value='hours']"),
+    minutes: document.querySelector("[data-value='mins']"),
+    seconds: document.querySelector("[data-value='secs']"),
+}
+class CountdownTimer{
+    constructor(countdownTimerObject){
+        this.selector = countdownTimerObject.selector;
+        this.targetDate = countdownTimerObject.targetDate;
+    }
+}
+const timer = new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Aug 28, 2022'),
+  });
+
+// let days;
+
+// let hours;
   
-  const logger = time => console.log(`Resolved after ${time}ms`);
+// let mins;
   
-  // Вызовы функции для проверки
-  delay(2000).then(logger); // Resolved after 2000ms
-  delay(1000).then(logger); // Resolved after 1000ms
-  delay(1500).then(logger); // Resolved after 1500ms
+// let secs;
+
+ const changeTime = () =>{
+    const realDate = new Date();
+    const time = timer.targetDate.getTime() - realDate.getTime();
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+      
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    //console.log(secs);
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.minutes.textContent = mins; 
+    refs.seconds.textContent = secs;
+ } 
+
+ const timerId = setInterval(changeTime, 1000);
+console.log('Days', days);
+console.log('Hours', hours);
+console.log('Minutes', mins);
+console.log('Seconds', secs);
